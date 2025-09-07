a31917fea2b33c8fb555dc8ba7204754b99068c4
Brylie Christopher Oxley <brylie@protonmail.com>
2025-09-07 15:42:50 +0300

    feat: update footer and navigation links to reflect current social media and music platforms

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Brylie Music Blog';
-export const SITE_DESCRIPTION = 'A blog about music, technology, and creative expression.';
+export const SITE_TITLE = 'Brylie Christopher music';
+export const SITE_DESCRIPTION = 'Featuring the musical creativity of Brylie Christopher.';
